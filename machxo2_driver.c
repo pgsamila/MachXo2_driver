bc9b8025de4ee193080562e1eec540fd0f281d55
pgsamila <pgsamila@yahoo.com>
2017-06-29 21:43:39 +0530

    Incomplete Update This commite have compilation errors with it, and the code is NOT COMPLETE!

--- machxo2_driver.c
+++ machxo2_driver.c
@@ -9,15 +9,16 @@
  * as published by the Free Software Foundation; version 3
  * of the License.
  */
-
 #include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/miscdevice.h>
+//#include <linux/init.h>
+//#include <linux/miscdevice.h>
 #include <linux/module.h>
-#include <asm/segment.h>
+//#include <asm/segment.h>
 #include <asm/uaccess.h>
 #include <linux/cdev.h>
-#include <linux/errno.h>
+//#include <linux/errno.h>
+
+#include <linux/i2c-dev.h>
 
 #define	DEVICENAME	"MachXo2"
 #define	TEXTLENGTH	100
@@ -32,6 +33,7 @@ static ssize_t machxo2_read(struct file *file, char *buf,
 static ssize_t machxo2_write(struct file *file, const char *buf, 
 				size_t count, loff_t *ppos);
 
+
 /* global variables */
 ssize_t machxo2_size;
 char machxo2_str[TEXTLENGTH];
@@ -42,14 +44,41 @@ int numofdevice = 1;
 dev_t dev;
 struct cdev cdev;
 
+
 /* file operations structure */
 static const struct file_operations machxo2_fops = {
-	.owner	 = THIS_MODULE,
-	.open	 = machxo2_open,
-	.release = machxo2_release,
-	.read	 = machxo2_read,
-	.write	 = machxo2_write
+	.owner	 	= THIS_MODULE,
+	.open	 	= machxo2_open,
+	.release	= machxo2_release,
+	.read	 	= machxo2_read,
+	.write	 	= machxo2_write
 };
+
+/* The I2C driver structure */
+static struct i2c_device_id machxo2_idtable[] = {
+	{"i2c-", 0},
+	{},
+}
+
+MODULE_DEVICE_TABLE(i2c, machxo2_idtable);
+
+static struct i2c_driver machxo2_i2c_driver = {
+	.driver = {
+		.name	= DEVICENAME,
+		.pm	= &machxo2_pm_ops,	/* optional */
+	},
+
+	.id_table	= machxo2_idtable,
+	.probe		= machxo2_probe,
+	.remove		= machxo2_remove,*/
+	/* if device autodetection is needed: */
+	.class		= I2C_CLASS_SOMETHING,
+	.detect		= machxo2_detect,
+	.address_list	= normal_i2c,
+
+	.shutdown	= machxo2_shutdown,	/* optional */
+	.command	= machxo2_command,/* optional, deprecated */
+}
 
 /*
  * machxo2_init - driver initializing at insmode
@@ -57,6 +86,7 @@ static const struct file_operations machxo2_fops = {
  */
 static int machxo2_init(void)
 {
+
 	/* register a range of char device numbers */
 	result = alloc_chrdev_region(&dev, minor, numofdevice, DEVICENAME); 
 	if(result < 0){
@@ -66,7 +96,7 @@ static int machxo2_init(void)
 	/* get major */
 	major = MAJOR(dev);
 	printk(KERN_ALERT "MachXo2: registered to major : %d\n",major);
-	
+
 	/* initialize a cdev structure */
 	cdev_init(&cdev, &machxo2_fops);
 
@@ -76,7 +106,6 @@ static int machxo2_init(void)
 		printk(KERN_ALERT "MachXo2: failed to add cdev\n");
 		return result;
 	}
-
 	printk(KERN_ALERT "MachXo2: initialization complete\n");
 	return 0;
 }
@@ -89,9 +118,10 @@ static void machxo2_exit(void)
 {
 	/* remove cdev structure */
 	cdev_del(&cdev);
-	
-	/* uregister a range of char device numbers */
+
+	/* unregister a range of char device numbers */
 	unregister_chrdev_region(dev, numofdevice);
+
 	printk(KERN_ALERT "MachXo2: exit called\n");
 }
 
@@ -101,6 +131,7 @@ static void machxo2_exit(void)
  */
 int machxo2_open(struct inode *inode, struct file *filp)
 {
+
         printk(KERN_ALERT "MachXo2: device opened\n");
         return 0;
 }
@@ -133,7 +164,7 @@ static ssize_t machxo2_read(struct file *file, char __user *buf,
 	/* Check the end position of data */
 	if(*ppos >= machxo2_size)
 		return 0;
-	
+
 	/* If user try to read more than available data */
 	if(*ppos + count > machxo2_size)
 		count = machxo2_size - *ppos;
@@ -167,7 +198,7 @@ static ssize_t machxo2_write(struct file *file, const char __user *buf,
 		/* send data from user */
 	        if(copy_from_user(machxo2_str, buf, count) != 0)
                 	return -EINVAL;
-		        	
+
 		*ppos = count;
 		printk(KERN_ALERT "MachXo2: Value written\n");
 	 	return count;
